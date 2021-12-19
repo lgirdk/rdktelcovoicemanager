aeee596ccbaef033e286586c82d0060e30e8fbea
Andre McCurdy <armccurdy@gmail.com>
2021-12-19 14:08:51 -0800

    fix missing declarations for COSA_MemoryCheck() etc
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/telcovoicemgr_dml_plugin_main.h
+++ source/TR-181/middle_layer_src/telcovoicemgr_dml_plugin_main.h
@@ -72,6 +72,10 @@ BOOL ANSC_EXPORT_API TELCOVOICEMGR_IsObjSupported(char* pObjName);
 
 void ANSC_EXPORT_API TELCOVOICEMGR_Unload(void);
 
+void ANSC_EXPORT_API COSA_MemoryCheck(void);
+void ANSC_EXPORT_API COSA_MemoryUsage(void);
+void ANSC_EXPORT_API COSA_MemoryTable(void);
+
 #ifdef __cplusplus
 }
 #endif
